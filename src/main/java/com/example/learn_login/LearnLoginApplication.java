9539e1f00982045ba861399d6e1566f7c543ba1d
luck1306 <luck1306@naver.com>
2022-09-21 23:12:28 +0900

    Initial commit

---
+++ src/main/java/com/example/learn_login/LearnLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLoginApplication.class, args);
+    }
+
+}
