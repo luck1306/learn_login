35d79f164b7223d15bea83095e18f7e5f98aa3fc
luck1306 <luck1306@naver.com>
2022-11-12 22:06:34 +0900

    update NotFoundExcpetion

--- src/main/java/com/example/learn_login/error/NotFoundException.java
+++ src/main/java/com/example/learn_login/error/NotFoundException.java
@@ -1,8 +1,15 @@
 package com.example.learn_login.error;
 
+import com.example.learn_login.error.exception.ErrorCode;
+import com.example.learn_login.error.exception.ProjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends ProjectException {
+    public static final ProjectException EXCEPTION =
+            new NotFoundException();
+    private NotFoundException() {
+        super(ErrorCode.NOT_FOUND);
+    }
 }
